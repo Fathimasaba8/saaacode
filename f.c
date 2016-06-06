da7cb8b6911c42553ebe45966bcf7cddeab76746
Fathimasaba8 <fathimasabhaa96@gmail.com>
2016-06-06 11:50:44 +0530

    Create f.c

---
+++ f.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+   int a;
+   scanf("%d",&a);
+ if(a%2==0)
+{
+printf("%d is even",a);
+}
+else
+{
+printf("%d is odd",a);
+}
+	return 0;
+}
